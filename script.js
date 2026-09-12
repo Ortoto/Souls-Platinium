515ed73a5e4d54c40ae7437d3463b012da499bfd
Ortoto <theo.palugan60800@gmail.com>
2026-09-12 22:20:41 +0200

    Elden Ring Page

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Salut")
+let bandeau = document.getElementById("bandeau_elden_ring");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+        bandeau.classList.add("cache");
+    } else {
+        bandeau.classList.remove("cache");
+    }
+});
